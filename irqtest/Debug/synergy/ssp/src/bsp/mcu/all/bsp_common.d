a1801df7b70c84175127cee272af2c81203a5dcb
GabrielSolisMabe <48843575+GabrielSolisMabe@users.noreply.github.com>
2019-08-10 11:03:39 -0500

    ejemplo1

---
+++ irqtest/Debug/synergy/ssp/src/bsp/mcu/all/bsp_common.d
@@ -0,0 +1,137 @@
+synergy/ssp/src/bsp/mcu/all/bsp_common.o: \
+ ../synergy/ssp/src/bsp/mcu/all/bsp_common.c \
+ C:\REP2\irqtest\synergy\ssp\inc\bsp/bsp_api.h \
+ c:\rep2\irqtest\synergy\ssp\inc\ssp_common_api.h \
+ c:\rep2\irqtest\synergy\ssp\inc\ssp_version.h \
+ C:\REP2\irqtest\synergy_cfg\ssp_cfg\bsp/bsp_cfg.h \
+ C:\REP2\irqtest\synergy_cfg\ssp_cfg\bsp/bsp_clock_cfg.h \
+ C:\REP2\irqtest\synergy_cfg\ssp_cfg\bsp/bsp_mcu_family_cfg.h \
+ C:\REP2\irqtest\synergy_cfg\ssp_cfg\bsp/bsp_mcu_device_pn_cfg.h \
+ C:\REP2\irqtest\synergy_cfg\ssp_cfg\bsp/bsp_mcu_device_cfg.h \
+ c:\rep2\irqtest\synergy\ssp\src\bsp\mcu\s7g2\bsp_mcu_info.h \
+ c:\rep2\irqtest\synergy\ssp\src\bsp\cmsis\device\renesas\s7g2\include\s7g2.h \
+ C:\REP2\irqtest\synergy\ssp\inc\bsp\cmsis\Include/core_cm4.h \
+ C:\REP2\irqtest\synergy\ssp\inc\bsp\cmsis\Include/cmsis_version.h \
+ C:\REP2\irqtest\synergy\ssp\inc\bsp\cmsis\Include/cmsis_compiler.h \
+ C:\REP2\irqtest\synergy\ssp\inc\bsp\cmsis\Include/cmsis_gcc.h \
+ C:\REP2\irqtest\synergy\ssp\inc\bsp\cmsis\Include/mpu_armv7.h \
+ c:\rep2\irqtest\synergy\ssp\src\bsp\cmsis\device\renesas\s7g2\include\system_S7G2.h \
+ c:\rep2\irqtest\synergy\ssp\src\bsp\mcu\s7g2\bsp_elc.h \
+ c:\rep2\irqtest\synergy\ssp\src\bsp\mcu\s7g2\bsp_cache.h \
+ c:\rep2\irqtest\synergy\ssp\src\bsp\mcu\s7g2\bsp_analog.h \
+ c:\rep2\irqtest\synergy\ssp\src\bsp\mcu\all\bsp_common_analog.h \
+ C:\REP2\irqtest\synergy_cfg\ssp_cfg\bsp/bsp_board_cfg.h \
+ c:\rep2\irqtest\synergy\board\s7g2_sk\bsp.h \
+ c:\rep2\irqtest\synergy\board\s7g2_sk\bsp_init.h \
+ c:\rep2\irqtest\synergy\board\s7g2_sk\bsp_leds.h \
+ c:\rep2\irqtest\synergy\board\s7g2_sk\bsp_qspi.h \
+ c:\rep2\irqtest\synergy\board\s7g2_sk\bsp_ethernet_phy.h \
+ c:\rep2\irqtest\synergy\ssp\src\bsp\mcu\all\bsp_common.h \
+ c:\rep2\irqtest\synergy\ssp\inc\ssp_common_api.h \
+ c:\rep2\irqtest\synergy\ssp\src\bsp\mcu\all\bsp_compiler_support.h \
+ c:\rep2\irqtest\synergy\ssp\src\bsp\mcu\all\bsp_register_protection.h \
+ c:\rep2\irqtest\synergy\ssp\src\bsp\mcu\all\bsp_locking.h \
+ c:\rep2\irqtest\synergy\ssp\src\bsp\mcu\all\bsp_irq.h \
+ c:\rep2\irqtest\synergy\ssp\src\bsp\mcu\all\bsp_group_irq.h \
+ c:\rep2\irqtest\synergy\ssp\src\bsp\mcu\all\bsp_clocks.h \
+ C:\REP2\irqtest\synergy\ssp\inc\driver\api/r_ioport_api.h \
+ c:\rep2\irqtest\synergy\ssp\inc\ssp_features.h \
+ c:\rep2\irqtest\synergy\ssp\inc\ssp_common_api.h \
+ c:\rep2\irqtest\synergy\ssp\src\bsp\mcu\all\bsp_compiler_support.h \
+ C:\REP2\irqtest\synergy\ssp\inc\driver\instances/r_fmi.h \
+ C:\REP2\irqtest\synergy\ssp\inc\driver\api/r_fmi_api.h \
+ c:\rep2\irqtest\synergy\ssp\src\bsp\mcu\all\bsp_common_leds.h \
+ c:\rep2\irqtest\synergy\ssp\src\bsp\mcu\all\bsp_delay.h \
+ c:\rep2\irqtest\synergy\ssp\src\bsp\mcu\all\bsp_feature.h \
+ c:\rep2\irqtest\synergy\ssp\src\bsp\mcu\all\bsp_mcu_api.h \
+ c:\rep2\irqtest\synergy\ssp\src\bsp\mcu\all\bsp_error_checking.h
+
+C:\REP2\irqtest\synergy\ssp\inc\bsp/bsp_api.h:
+
+c:\rep2\irqtest\synergy\ssp\inc\ssp_common_api.h:
+
+c:\rep2\irqtest\synergy\ssp\inc\ssp_version.h:
+
+C:\REP2\irqtest\synergy_cfg\ssp_cfg\bsp/bsp_cfg.h:
+
+C:\REP2\irqtest\synergy_cfg\ssp_cfg\bsp/bsp_clock_cfg.h:
+
+C:\REP2\irqtest\synergy_cfg\ssp_cfg\bsp/bsp_mcu_family_cfg.h:
+
+C:\REP2\irqtest\synergy_cfg\ssp_cfg\bsp/bsp_mcu_device_pn_cfg.h:
+
+C:\REP2\irqtest\synergy_cfg\ssp_cfg\bsp/bsp_mcu_device_cfg.h:
+
+c:\rep2\irqtest\synergy\ssp\src\bsp\mcu\s7g2\bsp_mcu_info.h:
+
+c:\rep2\irqtest\synergy\ssp\src\bsp\cmsis\device\renesas\s7g2\include\s7g2.h:
+
+C:\REP2\irqtest\synergy\ssp\inc\bsp\cmsis\Include/core_cm4.h:
+
+C:\REP2\irqtest\synergy\ssp\inc\bsp\cmsis\Include/cmsis_version.h:
+
+C:\REP2\irqtest\synergy\ssp\inc\bsp\cmsis\Include/cmsis_compiler.h:
+
+C:\REP2\irqtest\synergy\ssp\inc\bsp\cmsis\Include/cmsis_gcc.h:
+
+C:\REP2\irqtest\synergy\ssp\inc\bsp\cmsis\Include/mpu_armv7.h:
+
+c:\rep2\irqtest\synergy\ssp\src\bsp\cmsis\device\renesas\s7g2\include\system_S7G2.h:
+
+c:\rep2\irqtest\synergy\ssp\src\bsp\mcu\s7g2\bsp_elc.h:
+
+c:\rep2\irqtest\synergy\ssp\src\bsp\mcu\s7g2\bsp_cache.h:
+
+c:\rep2\irqtest\synergy\ssp\src\bsp\mcu\s7g2\bsp_analog.h:
+
+c:\rep2\irqtest\synergy\ssp\src\bsp\mcu\all\bsp_common_analog.h:
+
+C:\REP2\irqtest\synergy_cfg\ssp_cfg\bsp/bsp_board_cfg.h:
+
+c:\rep2\irqtest\synergy\board\s7g2_sk\bsp.h:
+
+c:\rep2\irqtest\synergy\board\s7g2_sk\bsp_init.h:
+
+c:\rep2\irqtest\synergy\board\s7g2_sk\bsp_leds.h:
+
+c:\rep2\irqtest\synergy\board\s7g2_sk\bsp_qspi.h:
+
+c:\rep2\irqtest\synergy\board\s7g2_sk\bsp_ethernet_phy.h:
+
+c:\rep2\irqtest\synergy\ssp\src\bsp\mcu\all\bsp_common.h:
+
+c:\rep2\irqtest\synergy\ssp\inc\ssp_common_api.h:
+
+c:\rep2\irqtest\synergy\ssp\src\bsp\mcu\all\bsp_compiler_support.h:
+
+c:\rep2\irqtest\synergy\ssp\src\bsp\mcu\all\bsp_register_protection.h:
+
+c:\rep2\irqtest\synergy\ssp\src\bsp\mcu\all\bsp_locking.h:
+
+c:\rep2\irqtest\synergy\ssp\src\bsp\mcu\all\bsp_irq.h:
+
+c:\rep2\irqtest\synergy\ssp\src\bsp\mcu\all\bsp_group_irq.h:
+
+c:\rep2\irqtest\synergy\ssp\src\bsp\mcu\all\bsp_clocks.h:
+
+C:\REP2\irqtest\synergy\ssp\inc\driver\api/r_ioport_api.h:
+
+c:\rep2\irqtest\synergy\ssp\inc\ssp_features.h:
+
+c:\rep2\irqtest\synergy\ssp\inc\ssp_common_api.h:
+
+c:\rep2\irqtest\synergy\ssp\src\bsp\mcu\all\bsp_compiler_support.h:
+
+C:\REP2\irqtest\synergy\ssp\inc\driver\instances/r_fmi.h:
+
+C:\REP2\irqtest\synergy\ssp\inc\driver\api/r_fmi_api.h:
+
+c:\rep2\irqtest\synergy\ssp\src\bsp\mcu\all\bsp_common_leds.h:
+
+c:\rep2\irqtest\synergy\ssp\src\bsp\mcu\all\bsp_delay.h:
+
+c:\rep2\irqtest\synergy\ssp\src\bsp\mcu\all\bsp_feature.h:
+
+c:\rep2\irqtest\synergy\ssp\src\bsp\mcu\all\bsp_mcu_api.h:
+
+c:\rep2\irqtest\synergy\ssp\src\bsp\mcu\all\bsp_error_checking.h:
